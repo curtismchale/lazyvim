14e504320a69840fcca24fda2d15997f106a81ce
Curtis McHale <curtis@curtismchale.ca>
2026-02-18 11:18:59 -0800

    Add theme configuration for Kanagawa and LazyVim

---
+++ lua/plugins/theme.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "rebelot/kanagawa.nvim",
+    priority = 1000,
+    opts = {
+      theme = "wave",
+      background = {
+        dark = "wave",
+        light = "lotus",
+      },
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "kanagawa-wave",
+    },
+  },
+}
